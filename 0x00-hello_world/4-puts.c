7e3f20ea628ba35c661d81ea41ea7a56e1aea00c
Tory-15 <toryrosalie@gmail.com>
2022-09-09 10:59:59 -0700

    4. Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  *main-Serves as the entry point for the program
+ *  main-Serves as the entry point for the program
  *
- *  *Return:By default returns zero if no erros otherwise returns non-zero value.
+ *  Return:By default returns zero if no erros otherwise returns non-zero value.
  *
  **/
 
