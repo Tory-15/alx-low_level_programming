644cf97cd3ceda3f5a2300c867b0164bf244dc92
Tory-15 <toryrosalie@gmail.com>
2022-09-09 11:06:52 -0700

    4. H ELLO  PUS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 /**
  *  main-Serves as the entry point for the program
  *  Return:By default returns zero if no erros otherwise returns non-zero value.
- **/
+ */
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 }
