af3789dc96ee09b8ee4ec057aaf9a11464dda24b
Tory-15 <toryrosalie@gmail.com>
2022-09-12 15:18:43 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,13 +4,15 @@
  * Return:Always 0 (success)
  */
 int main(void)
-{char ch = ('a'),
+{char ch = 'a',
 	while (ch <= 'z')
+	{
 	if (ch != 'e' && ch != 'q')
+	{
 	putchar(ch)
+	}
 	ch++;
-	putchar('\n');
-	ch++;
+	}
 	putchar('\n');
 	return (0);
 }
