efd5c1827c9cfd0724235e6339d631ca68707965
Tory-15 <toryrosalie@gmail.com>
2022-09-13 20:14:07 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+
+		_putchar ('+');
 		return (1);
 	}else if(n == 0)
 	{
-		_putchar(48);
+
+		_putchar (48);
 		return (0);
 	}else if(n < 0)
 	{
-		_putchar('-');
+
+		_putchar ('-');
 	}
 	return (-1);
 
