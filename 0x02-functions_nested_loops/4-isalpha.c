5670ce13123acab16054b81b86eff9f624ff14d5
Tory-15 <toryrosalie@gmail.com>
2022-09-13 19:21:26 -0700

    int _isalpha(int c)

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,7 @@
  *@c: the character to be checked
  *Return: 1 if c is a letter, 0 otherwise
  */
- int _isalpha(int c)
- 
- {
- 
- 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-	
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 	}
