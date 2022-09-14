d5b10c3de7a890f3aa3e101b29214a521a4ff23a
Tory-15 <toryrosalie@gmail.com>
2022-09-13 20:05:59 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,32 +9,22 @@
  *-1 and prints - if n is less than zero
  */
 int print_sign(int n)
-
 {
 	if (n > 0)
-
-				{
-
-							_putchar('+');
-
-									return (1);
-
-										} else if (n == 0)
-
-												{
-
-															_putchar(48);
-
-																	return (0);
-
-																		} else if (n < 0)
-
-																				{
-
-																							_putchar('-');
-
-																								}
-
-																				return (-1);
+	{
+		_putchar('+');
+		return (1);
+	}else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}else if (n < 0)
+	{
+		_putchar('-')
+	}
+	return (-1);
 
 }
+
+	
+
