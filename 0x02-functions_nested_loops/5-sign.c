8e48c930c04fc23bdf9d3f48da215365791f49d2
Tory-15 <toryrosalie@gmail.com>
2022-09-13 19:33:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,8 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  *-1 and prints - if n is less than zero
- */int print_sign(int n)
+ */
+int print_sign(int n)
 
 {
 
