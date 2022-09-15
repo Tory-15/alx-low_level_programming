04f4de4aa3aa4f666788ea60effd22e238970f9a
Tory-15 <toryrosalie@gmail.com>
2022-09-15 14:00:52 -0700

    awesome

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,29 +1,29 @@
-#include"main.h"
+#include "main.h"
 
 /**
-*largest _number-returns the largest of 3 numbers 
-*
-*@a: first integer
-*@b: second integer
-*@c: third integer
-*
-*Return: largest number 
-*/
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	if (a >= b && b >= c)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
+	}
 	else if (b >= a && b >= c)
-{
-	largest = b;
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-}
-
-else
-{
-	largest = c;
-}
-return (largest);
+	return (largest);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,3 @@
-0x03. C - Debugging
+# About Folder
+
+Starting to learn how to debug basic c codes from a given c-file.
