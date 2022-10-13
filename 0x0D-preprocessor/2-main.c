cb6a64c1a9166926f3c37409b7b6d8adfbcd36f5
Tory-15 <toryrosalie@gmail.com>
2022-10-12 21:30:04 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print source file name
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_LIKE_MICRO_H
+#define FUNCTION_LIKE_MICRO_H
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
